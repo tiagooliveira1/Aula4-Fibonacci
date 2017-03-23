a1fb4ffe97d11abff58ea96d0c91ed29a4657621
= <=>
2017-03-23 19:34:27 -0300

    Commit inicial

---
+++ src/com/opet/Aula4_Fibonacci.java
@@ -0,0 +1,20 @@
+package com.opet;
+
+public class Aula4_Fibonacci
+{
+
+    public static void main(String[] args)
+    {
+        // TODO Auto-generated method stub
+        int numeroAnterior = 0;
+        int numeroAtual = 1;
+        int somaAtual = 0;
+        while ((numeroAtual+numeroAnterior) < 10000) {
+            somaAtual = numeroAtual+numeroAnterior;
+            numeroAnterior = numeroAtual;
+            numeroAtual = somaAtual;
+            System.out.println(somaAtual);
+        }
+    }
+
+}
